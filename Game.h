302e01e117259bfbfc078475667aba9134c29027
AJ <dimezske@gmail.com>
2017-08-08 20:43:04 +0930

    Update Game.h

--- Game.h
+++ Game.h
@@ -2,6 +2,7 @@
 #include "SFML/Graphics.hpp"
 #include "SFML/Audio.hpp"
 #include <iostream>
+#include <unistd.h>
 
 using namespace std;
 
@@ -13,17 +14,38 @@ using namespace std;
 class Game
 {
 public:
+// Process Game
     Game();
-    void Player();
     void run();
+// Cameras
+    void view();
+
+// Hud Display
+    void hud();
+    void hudView();
+    void hudViewButtons();
+    void hudPos();
+    void invScrDimesions();
+    void invView();
+    void invClose();
+// Tileset
+    bool levelSet();
+    void Player();
     void drawPlayer(sf::RenderWindow &window);
     void setPlayerDir(int direction);
-    void loadPlayer();
+    int direction();
     void playerAnimation_WalkDown();
     void playerAnimation_WalkLeft();
     void playerAnimation_WalkRight();
     void playerAnimation_WalkUp();
+    void stopWalking();
 
+    // Objects
+    void statsBar();
+    void Button1();
+    void Shape();
+    // Environment
+    void bushGenerator();
 
 private:
     void processEvents();
@@ -31,5 +53,16 @@ private:
     void render();
     sf::RenderWindow window;
     sf::Clock clock2;
-    
+    sf::Clock clock3;
+    sf::Time time2;
+    sf::Texture background;
+    sf::Sprite bg;
+
+    int pDirection;
+    // Inventory
+    sf::Clock invClock;
+    sf::Time invTime;
+    // Button
+    sf::Sprite sprite;
+    sf::Texture texture;
 };
